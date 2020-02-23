603dcf01b97fb24a30e95b643af990809411d19e
Agnes Park <agnespark824@gmail.com>
2020-02-23 22:30:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears WHERE MIN(age);"
+  "SELECT bear.name, bear.age FROM bears ORDER BY age ASC LIM 1"
 end
 
 def selects_most_prominent_color_and_returns_with_count
